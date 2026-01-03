6e20c77736cde0f7245f49a530741b21e9a82222
Van de Sande Samuel <s.vandesande@student.helmo.be>
2026-01-03 20:21:09 +0100

    ajout de la page de contact pour devenir une marque plus suppression des liens vers log et moyen de livrvaison coté admin

--- src/features/navbar/menu/user/AdminMenu.tsx
+++ src/features/navbar/menu/user/AdminMenu.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Logs, Users, CreditCard, Truck, ListChecks } from "lucide-react";
+import { Users, CreditCard, ListChecks } from "lucide-react";
 
 export function AdminMenu() {
     const [, setOpen] = useState(false);
     const navigate = useNavigate();
 
     const menuItems = [
-        { icon: Logs, label: "Consulter les logs", path: "/admin/logs" },
         { icon: Users, label: "Gérer les comptes", path: "/admin/accounts" },
         { icon: CreditCard, label: "Moyens de paiement", path: "/admin/payments" },
-        { icon: Truck, label: "Moyens de livraison", path: "/admin/delivery" },
         { icon: ListChecks, label: "Gérer questionnaire éthique", path: "/admin/ethics" },
     ];
 
